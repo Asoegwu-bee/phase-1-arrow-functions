3fe43bef28f14c64e348b7848f3813e46bfb2aa4
Bianca Emmanuel <bianca.emmanuel@student.moringaschool.com>
2022-06-12 08:58:31 +0100

    Done

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function(num1, num2) {
+    return num1 / num2;
+};
+
+console.log(divide(2000, 100));
+
+const square = (x) => {
+    return x * 2;
+};
+console.log(square(2));
+
+const add = (a, b) => {
+    return a + b;
+};
+console.log(add(3, 4));
