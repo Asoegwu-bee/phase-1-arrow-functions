e56f8d27721c04bbd4d50109d3e4cb89c4dea10d
Bianca Emmanuel <bianca.emmanuel@student.moringaschool.com>
2022-06-12 08:57:19 +0100

    commit tree afa24d76bbb6118b3577c67c8d19e1f80578e690

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function(num1, num2) {
+    return num1 / num2;
+};
+
+console.log(divide(2000, 100));
+divide()
+
+const square = (x) => {
+    return x * 2;
+};
+console.log(square(2));
+
+const add = (a, b) => {
+    return a + b;
+};
+console.log(add(3, 4));
